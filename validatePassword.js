04e256b0cc380330c44335945f4d00e81edb064c
Snehal Chavan <78457966+snehalchavann@users.noreply.github.com>
2021-10-06 20:32:01 -0400

    Added test case module

--- validatePassword.js
+++ validatePassword.js
@@ -3,13 +3,29 @@ const client = require('./connection.js')
 
 // query database for user's password
 function validatePassword(username, password) {
-    return new Promise(function (resolve, reject) {
+    // return new Promise(function (resolve, reject) {
         var statement = "SELECT password FROM public.user where username = $1";
         var values = [username];
-
+        try{
         client.query(statement, values, function (err, res) {
-            // console.log(res);
-            var hash = res.rows[0].password;
+             console.log(res);
+            
+             try{
+                if(res.rows === undefined || res.rows == null || res.rows.length <= 0){
+                    console.log("......");
+                    
+                    throw new Api401Error('Authentication Failed')
+                
+                }
+             }catch(err){
+                 return false;
+                 console.log("error")
+             }
+             var hash = res.rows[0].password;
+                
+                
+            
+            
 
             //compare hash and password
             bcrypt.compare(password, hash, function (err, result) {
@@ -19,7 +35,10 @@ function validatePassword(username, password) {
                 return result
             })
         });
-    })
+    }catch(err){
+        console.log(err)
+    }
+    // })
 }
 
 module.exports = validatePassword
