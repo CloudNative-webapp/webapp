76e6b367cb9cbd82450fcfd413aa0e26fa064436
Snehal Chavan <78457966+snehalchavann@users.noreply.github.com>
2021-10-14 13:29:41 -0400

    updated changes

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ function emailIsValid(email) {
 
 
 //get all users
-app.get('/users', (req, res, next) => {
+app.get('/v1/user/self', (req, res, next) => {
 
     const base64Credentials = req.headers.authorization.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
@@ -49,13 +49,10 @@ app.get('/users', (req, res, next) => {
             client.query(`SELECT * FROM public.user WHERE username = $1`, values, (err, result) => {
                 if (err) {
                     console.log('Error' + error)
-                    // res.send(result.rows,res.statusCode);
-                    // res.status(res.statusCode).send(result.rows)
-
                 }
                 console.log('select query:' + result.rows)
-                delete result.rows[0]['password']
-                res.status(200).json(result.rows)
+                const {password,...cred} = result.rows[0]
+                res.status(200).json(cred)
             });
         } else {
             res.status(401).send({error: 'Authentication Failed'})
@@ -73,44 +70,39 @@ app.get('/users', (req, res, next) => {
 })
 
 //create a new user
-app.post('/createUser', (req, res) => {
+app.post('/v1/user', (req, res) => {
     let d = new Date();
     const userReq = req.body;
     var password = userReq.password;
-    let sqlString = `INSERT INTO public.user (username, password, first_name, last_name, account_created, account_updated) VALUES ($1, $2, $3, $4, $5, $6)`
+    let sqlString = `INSERT INTO public.user (username, password, first_name, last_name, account_created, account_updated) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *`
 
     try {
         if (!emailIsValid(userReq.username)) {
             res.status(400).send('Bad request')
-            throw new Api404Error('username not valid')
+            throw new Api400Error('username not valid')
         } else {
 
             //generate  a salt and hash the password
-            bcrypt.hash(password, saltRounds, function (err, hash) {
+            bcrypt.hash(password, saltRounds, async function (err, hash) {
                 const values = [userReq.username, hash, userReq.firstname, userReq.lastname, d, d];
-                client.query(sqlString, values, (err, result) => {
-                    // if(userReq.username == null){
-                    //     res.status()
-                    //     throw new Api404Error(`User with username: ${userReq.username} not found`)
-                    // }
-                    // else 
-                    try {
-                        if (err) {
-                            // res.status(400).send('Bad request')
+                    const ans = await client.query(sqlString, values)
 
-                            res.status(403).json({
-                                status: 403,
-                                error: "Username Already Exists"
+                    try {
+                        if (!ans) {
+                            res.status(400).json({
+                                status: 400,
+                                error: "Bad Request"
                             })
-                            throw new Api403Error(`Username: ${userReq.username}`)
+                            throw new Api400Error(`Username: ${userReq.username}`)
                         } else {
-                            res.status(201).json(result.rows)
+                            //omitted password field
+                             const {password,...result} = ans.rows[0]
+                            res.status(201).json(result)
                         }
                     } catch (error) {
                         console.log(error)
                     }
                 });
-            });
         }
     } catch (error) {
         console.log(error)
@@ -120,7 +112,7 @@ app.post('/createUser', (req, res) => {
 })
 
 //update details of user
-app.put('/users/updateUser', (req, res) => {
+app.put('/v1/user/self', (req, res) => {
     const base64Credentials = req.headers.authorization.split(' ')[1];
     const credentials = Buffer.from(base64Credentials, 'base64').toString('ascii');
     const [uname, passwd] = credentials.split(':');
@@ -129,19 +121,16 @@ app.put('/users/updateUser', (req, res) => {
         username,
         password,
         firstname,
-        lastname
+        lastname,
+        account_created,
+        account_updated
     } = req.body;
     let d = new Date();
-    console.log(firstname);
-
-    //let updateQuery = 'UPDATE public.user SET password = $1, first_name = $2, last_name = $3, account_updated = $4 WHERE username = $5'
-
+    
     let newquery = 'UPDATE public.user SET password = COALESCE(NULLIF($1, \'\'), password), first_name = COALESCE(NULLIF($2, \'\'), first_name), last_name = COALESCE(NULLIF($3, \'\'), last_name), account_updated = $4 WHERE username = $5'
 
     try {
-        if (req.body.accountCreated || req.body.accountUpdated) {
-            console.log('here')
-            // res.status(400).send('Bad request')
+        if (account_created || account_updated || username) {
             res.status(400).json({
                 status: 400,
                 error: 'Bad Request'
@@ -149,13 +138,10 @@ app.put('/users/updateUser', (req, res) => {
             throw new Api400Error(`Updated field not allowed`)
         } else if (validatePassword(uname, passwd)) {
 
-            // if(!passwordUser){
             bcrypt.hash(password, saltRounds, function (err, hash) {
-                const values = [hash, firstname, lastname, d, username];
+                const values = [hash, firstname, lastname, d, uname];
                 client.query(newquery, values, (err, result) => {
-                    if (result.status(200)) {
-                        // res.status(204).send('Update successful')
-                        // console.log(result);
+                    if (result) {
                         res.status(204).json({
                             status: 204,
                             message: "Update Successful"
@@ -168,7 +154,6 @@ app.put('/users/updateUser', (req, res) => {
             });
 
         } else {
-            // res.status(400).send('Bad request')
             res.status(401).json({
                 status: 401,
                 error: 'User is'
--- validatePassword.js
+++ validatePassword.js
@@ -2,17 +2,13 @@ const bcrypt = require('bcrypt');
 const client = require('./connection.js')
 
 // query database for user's password
-function validatePassword(username, password) {
-    // return new Promise(function (resolve, reject) {
+async function validatePassword(username, password) {
         var statement = "SELECT password FROM public.user where username = $1";
         var values = [username];
         try{
-        client.query(statement, values, function (err, res) {
-             console.log(res);
-            
+             const res = await client.query(statement, values)
              try{
                 if(res.rows === undefined || res.rows == null || res.rows.length <= 0){
-                    console.log("......");
                     
                     throw new Api401Error('Authentication Failed')
                 
@@ -22,23 +18,11 @@ function validatePassword(username, password) {
                  console.log("error")
              }
              var hash = res.rows[0].password;
-                
-                
             
-            
-
-            //compare hash and password
-            bcrypt.compare(password, hash, function (err, result) {
-                // execute code to test for access and login
-                console.log("in validate method: " + result)
-
-                return result
-            })
-        });
+            return await bcrypt.compare(password, hash)
     }catch(err){
         console.log(err)
     }
-    // })
 }
 
 module.exports = validatePassword
