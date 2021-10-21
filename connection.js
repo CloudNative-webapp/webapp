b177768e0b24f7731aa5010e080390063a2f6def
Snehal Chavan <78457966+snehalchavann@users.noreply.github.com>
2021-10-20 20:49:25 -0400

    edited connection file

--- connection.js
+++ connection.js
@@ -2,7 +2,7 @@ const {Client} = require('pg')
 
 const client = new Client({
     host: "localhost",
-    user: "newuser",
+    user: "postgres",
     port: 5432,
     password: "password",
     database: "postgres"
