8f1076405b6310370705c2486d06782290689cff
Snehal Chavan <78457966+snehalchavann@users.noreply.github.com>
2021-10-29 23:51:00 -0400

    added new apis

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const Api401Error = require('./api401Error')
 const Api400Error = require('./api400Error')
 const Api403Error = require('./api403Error')
 const validatePassword = require('./validatePassword')
+const fs = require("fs");
+const AWS = require('aws-sdk');
+const Jimp = require("jimp");
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
 
 //include bcrypt module
 const bcrypt = require('bcrypt');
@@ -17,15 +24,21 @@ app.listen(5000, () => {
     console.log("Sever is now listening at port 5000");
 })
 
-client.connect();
+client.connect(function(err) {
+    if (err) throw err;
+
+    client.query('CREATE DATABASE IF NOT EXISTS postgres;');
+    client.query('USE postgres;');
+    client.query('create table user(user_uid UUID DEFAULT uuid_generate_v4(),username VARCHAR(100) NOT NULL,password VARCHAR(100) NOT NULL,first_name VARCHAR(50) NOT NULL,last_name VARCHAR(50) NOT NULL,account_created timestamp with time zone,account_updated timestamp with time zone,PRIMARY KEY (user_uid);', function(error, result, fields) {
+        console.log("hi"+result);
+    });
+    client.end;
+});
 
 const bodyParser = require("body-parser"); //To access request body as JSON
-const {
-    error
-} = require('console');
-const {
-    application
-} = require('express');
+const { error } = require('console');
+const { application } = require('express');
+const { env } = require('process');
 app.use(bodyParser.json());
 
 
@@ -167,4 +180,41 @@ app.put('/v1/user/self', (req, res) => {
     client.end;
 })
 
-module.exports = app;
+app.post('/v1/user/self/pic', (req, res) => {
+    const value = [req.body.username]
+    // const userid="";
+    let sqlstr = `SELECT * FROM public.user WHERE username = $1`
+    async function getUserID(){
+        const ans = await client.query(sqlstr, value);
+        return ans.rows[0].user_uid;
+    }
+    getUserID().then((ans)=> userid = ans)
+    .catch(console.log("err"))
+    const imgBinary = req.body.data;
+    const buffer = Buffer.from(imgBinary, "base64");
+    const fname = Date.now()+".jpeg";
+    fs.writeFileSync(fname, buffer);
+    // fs.writeFileSync("new-path.jpg", buffer);
+    // const file = Jimp.read(buffer, (err, res) => {
+    //     if (err) throw new Error(err);
+    //     res.quality(5).write("resized.jpg");
+    //   });
+    
+      const folder = (req.body.username + "/");
+      const params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: (folder + fname),
+        ACL: 'public-read',
+        Body: fname
+      };
+
+      s3.upload(params, function(err, data) {
+        if (err) {
+            throw err;
+        }
+        console.log(`File uploaded successfully. ${data.Location}`);
+    });
+    
+})
+
+module.exports = app; 
