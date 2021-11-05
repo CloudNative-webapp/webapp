5f1b4b6b68fbbb52431b7bf29edfcbcd10456713
Snehal Chavan <78457966+snehalchavann@users.noreply.github.com>
2021-11-05 16:45:08 -0400

    new yml

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -2,5 +2,5 @@
 
 cd /home/ubuntu/webapp
 pwd
-# sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/home/ubuntu/webapp/webapp/amazon-cloudwatch-agent.json -s
+ls
 pm2 start app.js
