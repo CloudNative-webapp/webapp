1a1f5eef1715909b6d9d440e570902e180006854
Snehal Chavan <78457966+snehalchavann@users.noreply.github.com>
2021-11-06 10:09:57 -0400

    new changes

--- api401Error.js
+++ api401Error.js
@@ -1,15 +1,15 @@
 const httpStatusCodes = require('./httpStatusCodes')
-const BaseError = require('./BaseError')
+// const BaseError = require('./BaseError')
 
-class Api401Error extends BaseError {
-    constructor(
-        name,
-        statusCode = httpStatusCodes.UNAUTHORIZED,
-        description = 'UNAUTHORIZED',
-        isOperational = true
-    ) {
-        super(name, statusCode, isOperational, description)
-    }
-}
+// class Api401Error extends BaseError {
+//     constructor(
+//         name,
+//         statusCode = httpStatusCodes.UNAUTHORIZED,
+//         description = 'UNAUTHORIZED',
+//         isOperational = true
+//     ) {
+//         super(name, statusCode, isOperational, description)
+//     }
+// }
 
 module.exports = Api401Error
