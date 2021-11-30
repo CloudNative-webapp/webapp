e0d8eecbbd5e1ddd7ff627771fe39bd31d5948b7
Snehal Chavan <78457966+snehalchavann@users.noreply.github.com>
2021-11-30 16:26:51 -0500

    added new change

--- Actions/verifyUser.js
+++ Actions/verifyUser.js
@@ -1,5 +1,5 @@
-const client = require('./connection.js')
-const logger = require('./config/logger')
+const client = require('../connection.js')
+const logger = require('../config/logger')
 
 const verifyUser = (req,res) =>{
     logger.info('in verify user api')
