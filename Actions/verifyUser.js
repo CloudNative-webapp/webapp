791a047188bb5b235b4b393f04a6f3c79acce151
Snehal Chavan <78457966+snehalchavann@users.noreply.github.com>
2021-12-01 04:21:22 -0500

    added api

--- Actions/verifyUser.js
+++ Actions/verifyUser.js
@@ -3,7 +3,11 @@ const logger = require('../config/logger')
 
 const verifyUser = (req,res) =>{
     console.log(req, res);
+    logger.info('request 1st part',req.request);
+    logger.info('request 2nd part',req.request._parsedUrl);
+    
     const a = req.request._parsedUrl.query;
+    logger.info('request 3rd part',a);
     const username = a.split("=")[1].split("&")[0]
     const text = 'UPDATE public.users SET verified = $1, verified_on = $2 WHERE username =$3'
     const values = [true, new Date().toISOString(), username];
